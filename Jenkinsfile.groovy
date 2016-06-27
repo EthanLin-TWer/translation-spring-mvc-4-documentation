570827c7369bbc178de93ec74952323003f740aa
linesh <linesh.simpcity@gmail.com>
2016-06-27 18:27:13 +0800

    [Project] Add first jenkins stage

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,8 @@
+node ('main') {
+    stage 'Pull latest changes from SCM'
+    git({
+        url: 'git@github.com:linesh-simplicity/translation-spring-mvc-4-documentation.git'，
+        branch: 'master'
+    })
+    sh 'tree'
+}
