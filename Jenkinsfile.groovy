76b051b05772ba04b443424656eed71781a2496a
linesh <linesh.simpcity@gmail.com>
2016-06-27 18:30:57 +0800

    [Project] Fix syntax error in Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 node ('main') {
     stage 'Pull latest changes from SCM'
-    git({
+    git([
         url: 'git@github.com:linesh-simplicity/translation-spring-mvc-4-documentation.git'，
         branch: 'master'
-    })
+    ])
     sh 'tree'
 }
