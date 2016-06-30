ab8ee211fccb2a42519d995b5864957e7ec7583e
linesh <linesh.simpcity@gmail.com>
2016-06-30 13:17:53 +0800

    [#8] Update policy token function and do real upload to qiniu

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -12,18 +12,14 @@ console.log('QINIU_ACCESS_KEY: ' + qiniu.conf.ACCESS_KEY);
 console.log('QINIU_SECRET_KEY: ' + qiniu.conf.SECRET_KEY);
 console.log('BUCKET: ' + bucket);
 
-const uploadingBookDirectory = glob.sync('_book/**/*.*', {}).map(name => name.substring(0, 6))
+const uploadingBookDirectory = glob.sync('_book/**/*.*', {}).map(name => name.substring(6, name.length))
 console.log('all: ' + uploadingBookDirectory);
-//
-// all.foreach(file => {
-//     let key = file.substring(9, file.length);
-//     console.log('key:' + key);
-//     let token = uptoken(bucket, key);
-//     console.log('token:' + token);
-//     uploadFile(token, key, file)
-// })
 
-function uptoken(bucket, key) {
+all.foreach(key => {
+    uploadFile(policyToken(buckey, key), key, key)
+})
+
+function policyToken(bucket, key) {
   return new qiniu.rs.PutPolicy(bucket + ":" + key).token(); // ':' means allow override upload
 }
 
