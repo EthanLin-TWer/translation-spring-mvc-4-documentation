0308818a02f6c39af76f841e941ba99f6776d1f8
linesh <linesh.simpcity@gmail.com>
2016-06-30 12:40:45 +0800

    [#8] Qiniu component ready, a call to final hit

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -3,15 +3,47 @@ const fs = require('fs');
 const glob = require('glob');
 const crypto = require('crypto-js')
 
-let encryptedAccessKeyFromPipeline = process.argv.slice(2, 3);
-let encryptedSecretKeyFromPipeline = process.argv.slice(3, 4);
-let encryptor = process.argv.slice(4);
+let qiniuAccessKey = process.argv.slice(2, 3);
+let qiniuSecretKey = process.argv.slice(3);
 
-//需要填写你的 Access Key 和 Secret Key
-qiniu.conf.ACCESS_KEY = crypto.AES.decrypt(encryptedAccessKeyFromPipeline, encryptor).toString(crypto.enc.Utf8);
-qiniu.conf.SECRET_KEY = crypto.AES.decrypt(encryptedSecretKeyFromPipeline, encryptor).toString(crypto.enc.Utf8);
+qiniu.conf.ACCESS_KEY = qiniuAccessKey;
+qiniu.conf.SECRET_KEY = qiniuSecretKey;
 bucket = 'mvc-linesh-tw';
 
 console.log('QINIU_ACCESS_KEY: ' + qiniu.conf.ACCESS_KEY);
 console.log('QINIU_SECRET_KEY: ' + qiniu.conf.SECRET_KEY);
-console.log('ENCRYPTOR: ' + encryptor);
+console.log('BUCKET: ' + bucket);
+
+
+function uptoken(bucket, key) {
+  return new qiniu.rs.PutPolicy(bucket + ":" + key).token(); // ':' means allow override upload
+}
+
+
+//构造上传函数
+function uploadFile(uptoken, key, localFile) {
+  var extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
+      if(!err) {
+        // 上传成功， 处理返回值
+        console.log('=====================Upload Success======================');
+        console.log(ret.hash, ret.key, ret.persistentId);
+      } else {
+        // 上传失败， 处理返回代码
+        console.log('==========================Boom===========================');
+        console.log(err);
+      }
+  });
+}
+
+
+glob('../_book/**/*.*', {}, (error, files) => {
+    files.map(file => {
+        let key = file.substring(9, file.length);
+        console.log('key:' + key);
+        let token = uptoken(bucket, key);
+        console.log('token:' + token);
+        uploadFile(token, key, file)
+    })
+
+})
