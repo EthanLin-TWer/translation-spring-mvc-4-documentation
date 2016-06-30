401096aa47ebad1f143dbc6b116233c7145d9d1c
linesh <linesh.simpcity@gmail.com>
2016-06-30 19:29:07 +0800

    [#8] Code refactor, prettifier success & error log

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -6,36 +6,31 @@ const crypto = require('crypto-js')
 let qiniuAccessKey = process.argv.slice(2, 3);
 let qiniuSecretKey = process.argv.slice(3);
 
+// Prepare Qiniu configuration options
 qiniu.conf.ACCESS_KEY = qiniuAccessKey.toString(crypto.enc.Utf8);;
 qiniu.conf.SECRET_KEY = qiniuSecretKey.toString(crypto.enc.Utf8);;
 bucket = 'mvc-linesh-tw';
 
-const uploadingBookDirectoryFiles = glob.sync('_book/**/*.*', {})
-
-uploadingBookDirectoryFiles.forEach(filename => {
+glob.sync('_book/**/*.*', {}).forEach(filename => {
     const resource_key_in_qiniu_api = filename.substring('_book/'.length, filename.length);
+    const policyToken = policyToken(bucket, resource_key_in_qiniu_api);
 
-    const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
     uploadFile(policyToken, resource_key_in_qiniu_api, filename)
 })
 
 function policyToken(bucket, key) {
-  return new qiniu.rs.PutPolicy(bucket + ":" + key).token(); // ':' means allow override upload
+    // ':' means allow override upload. For further details refer to offical API docs
+    return new qiniu.rs.PutPolicy(bucket + ":" + key).token();
 }
 
 //构造上传函数
 function uploadFile(uptoken, key, localFile) {
-    var extra = new qiniu.io.PutExtra();
-    console.log('------------- Uploading file -------------')
-    qiniu.io.putFile(uptoken, key, localFile, extra, function(err, ret) {
-      if(!err) {
-        // 上传成功， 处理返回值
-        console.log('=====================Upload Success======================');
-        console.log(ret.hash, ret.key, ret.persistentId);
-      } else {
-        // 上传失败， 处理返回代码
-        console.log('==========================Boom===========================');
-        console.log(err);
-      }
+    let extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function(error, response) {
+        if(!err) {
+            console.log('[Success] File uploaded: ' + response.key);
+        } else {
+            console.log(error);
+        }
     });
 }
