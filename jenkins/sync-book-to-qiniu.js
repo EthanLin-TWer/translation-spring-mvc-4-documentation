c9604b39c5331b67625bc2b9c75a93aede783468
linesh <linesh.simpcity@gmail.com>
2016-06-30 12:08:35 +0800

    [#8] Still working on fetching AK and SK from pipeline

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -3,10 +3,8 @@ let fs = require('fs');
 let glob = require('glob');
 let crypto = require('crypto-js')
 
-let qiniukeys = process.argv.slice(2).split(',');
-
-let accessKey = qiniukeys[0];
-let secretKey = qiniukeys[1];
+let accessKey = process.argv.slice(2, 3);
+let secretKey = process.argv.slice(3);
 
 console.log('ak: ' + accessKey);
 console.log('sk: ' + secretKey);
