85ec9702d33204a3a5475400deccb3387c116469
linesh <linesh.simpcity@gmail.com>
2016-06-30 12:34:44 +0800

    [#8] Try to access the keys directly instead of passing through function

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -1,20 +1,16 @@
-let qiniu = require("qiniu");
-let fs = require('fs');
-let glob = require('glob');
-let crypto = require('crypto-js')
+const qiniu = require("qiniu");
+const fs = require('fs');
+const glob = require('glob');
+const crypto = require('crypto-js')
 
 let encryptedAccessKeyFromPipeline = process.argv.slice(2, 3);
 let encryptedSecretKeyFromPipeline = process.argv.slice(3, 4);
 let encryptor = process.argv.slice(4);
 
 //需要填写你的 Access Key 和 Secret Key
-qiniu.conf.ACCESS_KEY = decrypt(crypto, encryptedAccessKeyFromPipeline, encryptor);
-qiniu.conf.SECRET_KEY = decrypt(crypto, encryptedSecretKeyFromPipeline, encryptor);
+qiniu.conf.ACCESS_KEY = crypto.AES.decrypt(encryptedAccessKeyFromPipeline, encryptor).toString(crypto.enc.Utf8);
+qiniu.conf.SECRET_KEY = crypto.AES.decrypt(encryptedSecretKeyFromPipeline, encryptor).toString(crypto.enc.Utf8);
 bucket = 'mvc-linesh-tw';
-
-function decrypt(crypto, qiniu_key, encrytpor) {
-    return crypto.AES.decrypt(qiniu_key, encryptor).toString(crypto.enc.Utf8);
-}
 
 console.log('QINIU_ACCESS_KEY: ' + qiniu.conf.ACCESS_KEY);
 console.log('QINIU_SECRET_KEY: ' + qiniu.conf.SECRET_KEY);
