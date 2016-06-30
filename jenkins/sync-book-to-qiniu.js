78488110c961b49924c686cee1593b89e19be8f8
linesh <linesh.simpcity@gmail.com>
2016-06-30 21:18:48 +0800

    [#8] Filter some files out not uploading to qiniu

--- jenkins/sync-book-to-qiniu.js
+++ jenkins/sync-book-to-qiniu.js
@@ -2,20 +2,32 @@ const qiniu  = require("qiniu");
 const glob   = require('glob');
 const crypto = require('crypto-js')
 
+const ignoredFiles = [
+    'Jenkinsfile.groovy',
+    'sync-book-to-qiniu.js',
+    'sync-book-to-qiniu.sh',
+    'mvc-origin.md',
+    'package.json'
+];
+
 // node ./jenkins/sync-book-to-qiniu.js $ACCESS_KEY $SECRET_KEY
 let qiniuAccessKey = process.argv.slice(2, 3);
 let qiniuSecretKey = process.argv.slice(3);
 
 // Prepare Qiniu configuration options
-qiniu.conf.ACCESS_KEY = qiniuAccessKey.toString(crypto.enc.Utf8);;
-qiniu.conf.SECRET_KEY = qiniuSecretKey.toString(crypto.enc.Utf8);;
+qiniu.conf.ACCESS_KEY = qiniuAccessKey.toString(crypto.enc.Utf8);
+qiniu.conf.SECRET_KEY = qiniuSecretKey.toString(crypto.enc.Utf8);
 bucket = 'mvc-linesh-tw';
 
-glob.sync('_book/**/*.*', {}).forEach(filename => {
+glob.sync('_book/**/*.*', {}).filter(filename => {
+    for (let ignored of ignoredFiles) {
+        if (filename.endsWith(ignored)) return false;
+    }
+    return true;
+}).forEach(filename => {
     const resource_key_in_qiniu_api = filename.substring('_book/'.length, filename.length);
     // ':' means allow override upload. For further details refer to offical API docs
     const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
-
     uploadFile(policyToken, resource_key_in_qiniu_api, filename)
 })
 
