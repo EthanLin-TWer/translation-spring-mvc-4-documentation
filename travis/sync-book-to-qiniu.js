08a1c3bb22380c57b1b6415a78bba118e9dac91c
linesh <linesh.simpcity@gmail.com>
2016-07-05 21:56:18 +0800

    [#21] Repoint all automation to travis folder and do real uploading with travis scripts

--- travis/sync-book-to-qiniu.js
+++ travis/sync-book-to-qiniu.js
@@ -4,45 +4,41 @@ const crypto = require('crypto-js')
 
 const ignoredFiles = [
     'Jenkinsfile.groovy',
+    '.travis.yml',
     'sync-book-to-qiniu.js',
     'sync-book-to-qiniu.sh',
     'mvc-origin.md',
     'package.json'
 ];
 
-// node ./jenkins/sync-book-to-qiniu.js $ACCESS_KEY $SECRET_KEY
+// node ./travis/sync-book-to-qiniu.js $ACCESS_KEY $SECRET_KEY
 let qiniuAccessKey = process.argv.slice(2, 3);
 let qiniuSecretKey = process.argv.slice(3);
 
-console.log('ak: ' + qiniuAccessKey);
-console.log('sk: ' + qiniuSecretKey);
+// Prepare Qiniu configuration options
+qiniu.conf.ACCESS_KEY = qiniuAccessKey.toString(crypto.enc.Utf8);
+qiniu.conf.SECRET_KEY = qiniuSecretKey.toString(crypto.enc.Utf8);
+bucket = 'mvc-linesh-tw';
 
-console.log('Travis env infrastructure works just fine');
-//
-// // Prepare Qiniu configuration options
-// qiniu.conf.ACCESS_KEY = qiniuAccessKey.toString(crypto.enc.Utf8);
-// qiniu.conf.SECRET_KEY = qiniuSecretKey.toString(crypto.enc.Utf8);
-// bucket = 'mvc-linesh-tw';
-//
-// glob.sync('_book/**/*.*', {}).filter(filename => {
-//     for (let ignored of ignoredFiles) {
-//         if (filename.endsWith(ignored)) return false;
-//     }
-//     return true;
-// }).forEach(filepath => {
-//     const resource_key_in_qiniu_api = filepath.substring('_book/'.length, filepath.length);
-//     // ':' means allow override upload. For further details refer to offical API docs
-//     const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
-//     uploadFile(policyToken, resource_key_in_qiniu_api, filepath)
-// })
-//
-// function uploadFile(uptoken, key, localFile) {
-//     let extra = new qiniu.io.PutExtra();
-//     qiniu.io.putFile(uptoken, key, localFile, extra, function(error, response) {
-//         if(!error) {
-//             console.log('[Success] File uploaded: ' + response.key);
-//         } else {
-//             console.log(error);
-//         }
-//     });
-// }
+glob.sync('_book/**/*.*', {}).filter(filename => {
+    for (let ignored of ignoredFiles) {
+        if (filename.endsWith(ignored)) return false;
+    }
+    return true;
+}).forEach(filepath => {
+    const resource_key_in_qiniu_api = filepath.substring('_book/'.length, filepath.length);
+    // ':' means allow override upload. For further details refer to offical API docs
+    const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
+    uploadFile(policyToken, resource_key_in_qiniu_api, filepath)
+})
+
+function uploadFile(uptoken, key, localFile) {
+    let extra = new qiniu.io.PutExtra();
+    qiniu.io.putFile(uptoken, key, localFile, extra, function(error, response) {
+        if(!error) {
+            console.log('[Success] File uploaded: ' + response.key);
+        } else {
+            console.log(error);
+        }
+    });
+}
