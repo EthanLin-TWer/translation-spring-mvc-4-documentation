80adef1997cef31b1d493781babff17a0c1524c7
linesh <linesh.simpcity@gmail.com>
2016-08-25 22:57:30 +0800

    [#8] Add gulp watch task to auto update css/stylus when developing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const watch = require('gulp-watch');
 
-gulp.task('css', function () {
+gulp.task('stylus', function () {
   return gulp.src('./css/*.styl')
     .pipe(stylus({ compress: true }))
     .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('default', [ 'css' ])
+gulp.task('watch', ['stylus'], function () {
+    gulp.watch('./css/*.styl' , ['stylus']);
+});
+
+gulp.task('default', [ 'stylus', 'watch' ])
