89d5a33b6bdd03795f9d59cea86a7039200a82bc
linesh <linesh.simpcity@gmail.com>
2016-08-26 11:58:08 +0800

    [#6] Use gulp-maekdon to convert markdown before insertion to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
-const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-const watch = require('gulp-watch');
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const markdown = require('gulp-markdown')
+const watch = require('gulp-watch')
 
 gulp.task('stylus', function () {
-  return gulp.src('./css/*.styl')
-    .pipe(stylus({ compress: true }))
-    .pipe(gulp.dest('./dist/css/'));
-});
+    return gulp.src('./css/*.styl')
+        .pipe(stylus({ compress: true }))
+        .pipe(gulp.dest('./dist/css/'))
+})
+
+gulp.task('markdown-to-html', function() {
+    return gulp.src('./publish/**/*.md')
+        .pipe(markdown())
+        .pipe(gulp.dest('./dist/build/'))
+})
 
 gulp.task('watch', ['stylus'], function () {
-    gulp.watch('./css/*.styl' , ['stylus']);
-});
+    gulp.watch('./css/*.styl' , ['stylus'])
+})
 
 gulp.task('default', [ 'stylus', 'watch' ])
+gulp.task('build', [ 'stylus', 'markdown-to-html' ])
