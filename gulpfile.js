43dc42ad7fad7ff1763e6716887d8b4d7cbeb1b8
linesh <linesh.simpcity@gmail.com>
2016-08-26 22:20:05 +0800

    [#6] Add watch support for stylus in npm and thus remove those in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 const gulp = require('gulp')
-const stylus = require('gulp-stylus')
 const markdown = require('gulp-markdown')
-const watch = require('gulp-watch')
 const fs = require('fs')
 const glob = require('glob')
 
@@ -44,22 +42,10 @@ function extractDocs() {
 extractDocs()
 
 
-gulp.task('stylus', function () {
-    return gulp.src('./css/*.styl')
-        .pipe(stylus({ compress: true }))
-        .pipe(gulp.dest('./dist/css/'))
-})
-
 gulp.task('markdown-to-html', function() {
     return gulp.src('./publish/**/*.md')
         .pipe(markdown())
         .pipe(gulp.dest('./dist/build/'))
 })
 
-gulp.task('watch', ['stylus'], function () {
-    gulp.watch('./css/*.styl' , ['stylus'])
-})
-
-gulp.task('default', [ 'markdown-to-html', 'stylus', 'watch' ])
-gulp.task('style', [ 'stylus', 'watch' ])
-gulp.task('build', [ 'markdown-to-html', 'stylus' ])
+gulp.task('default', [ 'markdown-to-html' ])
