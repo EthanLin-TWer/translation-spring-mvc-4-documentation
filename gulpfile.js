7ab241922e20348ca878ada881a915b850bcbb2d
linesh <linesh.simpcity@gmail.com>
2016-08-27 01:54:16 +0800

    [#6] Update interpolation script and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 const fs = require('fs')
 const glob = require('glob')
+
 const PREV_PRIOR = -1
 const NEXT_PRIOR = 1
+const MARKER_INTERPOLATION_START = '<!-- interpolation-start -->'
+const MARKER_INTERPOLATION_END = '<!-- interpolation-end -->'
 
-// dist/build directory built by npm run markdown2html or node build/markdown-to-html.js
-const all = glob.sync('dist/build/**/*.html', {}).sort(inTableOfContentOrder)
+const all = sortInsertingHtmlsInTableOfContentOrder()
+removePreviousDocumentationContents()
+updateNewDocumentContent(all)
+
+function sortInsertingHtmlsInTableOfContentOrder() {
+    // dist/build directory pre-built by 'npm run markdown2html' or 'node build/markdown-to-html.js'
+    // this array should be sorted in the order they are inserted to the index.html(i.e the table of content order)
+    const all = glob.sync('dist/build/**/*.html', {}).sort(inTableOfContentOrder)
+    console.log('order to be inserted to the \'index.html\' file: ')
+    console.log(all)
+
+    return all
+}
 
 function inTableOfContentOrder(prev, next) {
     if (sectionNumber(prev) !== sectionNumber(next)) {
@@ -32,18 +46,21 @@ function subSectionNumber(section) {
     // extract sub-section number such like: /21-2/2-xxx.md, /21-13/1-xxx.md
     return Number(/\/21-\d{1,2}\/(\d+)/gi.exec(section)[1])
 }
-const temp = fs.readFileSync('index.html', 'utf-8')
-let interp_start = temp.indexOf('<div class="documentation-main chapter">') + '<div class="documentation-main chapter">'.length
-let interp_end = temp.indexOf('<!-- interpolation -->')
-let removing = temp.substring(interp_start, interp_end)
-fs.writeFileSync('index.html', temp.replace(removing, ''), 'utf-8')
 
-console.log(all)
-
-for (let i = 0; i < all.length; i++) {
-    file = all[i];
-    const content = fs.readFileSync(file, 'utf-8')
+function removePreviousDocumentationContents() {
     const index = fs.readFileSync('index.html', 'utf-8')
-            .replace('<!-- interpolation -->', '<section>' + content + '</section' + '<!-- interpolation -->')
-    fs.writeFileSync('index.html', index, 'utf-8')
+    let interp_start = index.indexOf(MARKER_INTERPOLATION_START) + MARKER_INTERPOLATION_START.length
+    let interp_end = index.indexOf(MARKER_INTERPOLATION_END)
+    let contents = index.substring(interp_start, interp_end)
+
+    fs.writeFileSync('index.html', index.replace(contents, ''), 'utf-8')
+}
+
+function updateNewDocumentContent(all) {
+    for (let i = 0; i < all.length; i++) {
+        const content = fs.readFileSync(all[i], 'utf-8')
+        const index = fs.readFileSync('index.html', 'utf-8')
+                .replace(MARKER_INTERPOLATION_END, '<section>' + content + '</section>' + MARKER_INTERPOLATION_END)
+        fs.writeFileSync('index.html', index, 'utf-8')
+    }
 }
