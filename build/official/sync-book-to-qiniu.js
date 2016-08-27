073c08dffe8ddfdd3c1f9814f54a6974939112aa
linesh <linesh.simpcity@gmail.com>
2016-08-28 03:05:11 +0800

    [#6] Upload seems to be working fine, small final tweaks

--- build/official/sync-book-to-qiniu.js
+++ build/official/sync-book-to-qiniu.js
@@ -9,7 +9,7 @@ qiniu.conf.SECRET_KEY = process.argv.slice(3).toString(crypto.enc.Utf8);
 const bucket = 'mvc-linesh-tw';
 
 glob.sync('dist/**/*.*', {}).filter(filename => !filename.startsWith('dist/build/')).forEach(filepath => {
-    const resource_key_in_qiniu_api = filepath.startsWith('dist/figures') ? filepath.substring('dist/'.length, filepath.length) : filepath;
+    const resource_key_in_qiniu_api = (filepath.startsWith('dist/figures') || filepath.startsWith('dist/index.html')) ? filepath.substring('dist/'.length, filepath.length) : filepath;
     // console.log(resource_key_in_qiniu_api);
     // ':' means allow override upload. For further details refer to offical API docs
     const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
