d56358c23c7f51bd7c9f7443d738c8b2f8df6700
linesh <linesh.simpcity@gmail.com>
2016-08-27 13:35:04 +0800

    [#6] Remove copied markdown since content already fetched

--- build/markdown-to-html.js
+++ build/markdown-to-html.js
@@ -5,7 +5,8 @@ const marked = require('marked')
 glob.sync('dist/build/**/*', {}).filter(file => file.endsWith('.md')).forEach(md => {
     fse.readFile(md, 'utf-8', (error, content) => {
         fse.outputFile(md.replace('.md', '.html'), marked(content, { gfm: true }), 'utf-8')
+        fse.remove(md)
 
-        return console.log('[html]: ' + md + ' converted to html')
+        return console.log('[html]: ' + md + ' converted to html, origin markdown removed')
     })
 })
