baadc7ae5ae4eb916a355ea1777375a8b11c16d1
linesh <linesh.simpcity@gmail.com>
2016-08-27 12:56:28 +0800

    [#6] Refactor markdown-to-html script, format logs, clean up

--- build/markdown-to-html.js
+++ build/markdown-to-html.js
@@ -4,22 +4,22 @@ const marked = require('marked')
 
 glob.sync('publish/**/*', {}).forEach(md => {
     if (['jpg', 'png', 'jpeg'].find(pic_format => md.endsWith(pic_format))) {
-        console.log('[image]: ' + md + ', copying to destination...')
         fse.copy(md, md.replace('publish', 'dist/build'))
-        return
+        return console.log('[image]: ' + md + ', copying to destination...')
     }
 
     if (fse.statSync(md).isDirectory()) {
-        console.log('[directory]: ' + md + ', returning...')
-        return
+        return console.log('[directory]: ' + md + ', returning...')
     }
 
-    console.log('[markdown]: ' + md + ', copying to destination...')
     fse.readFile(md, 'utf-8', (error, content) => {
-        if (error) return console.error('error occured when reading file \'' + md + '\' using fs-extra, ' +
-            'detailed message below:\n' + error + '\n')
+        if (error) {
+            return console.error('error occured when reading file \'' + md + '\' using fs-extra, ' +
+                                 'detailed message below:\n' + error + '\n')
+        }
 
         fse.outputFile(md.replace('publish', 'dist/build').replace('.md', '.html'),
             marked(content, { gfm: true }), 'utf-8');
+        return console.log('[markdown]: ' + md + ', copying to destination...')
     })
 })
