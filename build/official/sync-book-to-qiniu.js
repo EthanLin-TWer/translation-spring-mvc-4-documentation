8381c3d2c4847a831eca17d4104502ff395eb03f
linesh <linesh.simpcity@gmail.com>
2016-08-28 10:02:40 +0800

    [#6]

--- build/official/sync-book-to-qiniu.js
+++ build/official/sync-book-to-qiniu.js
@@ -8,13 +8,15 @@ qiniu.conf.ACCESS_KEY = process.argv.slice(2, 3).toString(crypto.enc.Utf8);
 qiniu.conf.SECRET_KEY = process.argv.slice(3).toString(crypto.enc.Utf8);
 const bucket = 'mvc-linesh-tw';
 
+console.log('---------------------------')
 glob.sync('dist/**/*.*', {}).filter(filename => !filename.startsWith('dist/build/')).forEach(filepath => {
     const resource_key_in_qiniu_api = filepath.substring('dist/'.length, filepath.length);
-    console.log(resource_key_in_qiniu_api);
+    // console.log(resource_key_in_qiniu_api);
     // ':' means allow override upload. For further details refer to offical API docs
     const policyToken = new qiniu.rs.PutPolicy(bucket + ":" + resource_key_in_qiniu_api).token();
-    // uploadFile(policyToken, resource_key_in_qiniu_api, filepath)
+    uploadFile(policyToken, resource_key_in_qiniu_api, filepath)
 })
+console.log('---------------------------')
 
 function uploadFile(uptoken, key, localFile) {
     let extra = new qiniu.io.PutExtra();
