2a207cd0cd4a93b029fe4309852065a6a30b4ac1
linesh <linesh.simpcity@gmail.com>
2016-09-02 01:30:33 +0800

    [#27] Fix path issue

--- build/clear-bucket-before-uploading.js
+++ build/clear-bucket-before-uploading.js
@@ -1,6 +1,6 @@
-const qiniuUtils = require('../utils/qiniu.js')
+const qiniuUtils = require('./utils/qiniu.js')
 
-// node ./build/official/clear-bucket-before-uploading.js $ACCESS_KEY $SECRET_KEY
+// node ./build/clear-bucket-before-uploading.js $ACCESS_KEY $SECRET_KEY
 // Prepare Qiniu configuration options
 accessKey = process.argv.slice(2, 3)
 secretKey = process.argv.slice(3)
