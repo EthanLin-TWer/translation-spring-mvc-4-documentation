30f5eb97c91ce88bf7ffb46826ad6902f03982e8
linesh <linesh.simpcity@gmail.com>
2016-09-02 00:51:45 +0800

    [#27] Remove file at the very beginning to avoid concurrent issue

--- build/official/clear-bucket-before-uploading.js
+++ build/official/clear-bucket-before-uploading.js
@@ -1,14 +1,11 @@
 const qiniuUtils = require('../utils/qiniu.js')
 
-// node ./build/official/sync-book-to-qiniu.js $ACCESS_KEY $SECRET_KEY
+// node ./build/official/clear-bucket-before-uploading.js $ACCESS_KEY $SECRET_KEY
 // Prepare Qiniu configuration options
 accessKey = process.argv.slice(2, 3)
 secretKey = process.argv.slice(3)
 
-qiniuUtils.uploadFiles('dist/**', {
+qiniuUtils.clearBucket({
    accessKey: accessKey,
    secretKey: secretKey,
-   strippedPath: 'dist/',
-   ignoringList: [ 'dist/build/**' ],
-   nodir: true
 })
